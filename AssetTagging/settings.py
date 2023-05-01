284bec9c6b458bcfb6fa4878c557acd808353ed9
Sherab <12190023.gcit@rub.edu.bt>
2023-05-01 23:01:39 +0600

    build.sh file configuration

--- AssetTagging/settings.py
+++ AssetTagging/settings.py
@@ -141,18 +141,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'static'),
     os.path.join(BASE_DIR, 'backend', 'static'),
     
 ]
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
