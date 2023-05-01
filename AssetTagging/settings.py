c74037df985ebcc47e52a160b15f7ccfa8e3ba54
Sherab <12190023.gcit@rub.edu.bt>
2023-05-01 22:20:21 +0600

    whitenoise installed

--- AssetTagging/settings.py
+++ AssetTagging/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'frontend',
     'backend',
     'django.contrib.admin',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
