a3291c7be594e766613a6d836fccf5b5a6d1f380
Sherab <12190023.gcit@rub.edu.bt>
2023-05-24 10:17:00 +0600

    api to get asset by owner

--- AssetTagging/settings.py
+++ AssetTagging/settings.py
@@ -50,16 +50,21 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'cloudinary_storage',
     'rest_framework',
+    'django_filters',
     'rest_framework.authtoken',
 
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-     'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 
--- api/views.py
+++ api/views.py
@@ -1,15 +1,31 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions,filters
 from rest_framework.authentication import TokenAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
+
 from backend.models import Asset
 from api.serializers import DormAmenitiesAssetSerializer
 
+# class DormAmenitiesAssetList(generics.ListAPIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [permissions.IsAuthenticated]
+#     serializer_class = DormAmenitiesAssetSerializer
+
+#     def get_queryset(self):
+#         return Asset.objects.filter(category__category_name='dorm_amenities')
 class DormAmenitiesAssetList(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = DormAmenitiesAssetSerializer
+    queryset = Asset.objects.filter(category__category_name='dorm_amenities')
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name', 'location', 'owner']
+    ordering_fields = ['name']
+    filterset_fields = ['owner']  # Add this line to enable filtering by owner
 
     def get_queryset(self):
-        return Asset.objects.filter(category__category_name='dorm_amenities')
+        queryset = super().get_queryset()
+        # Apply additional filtering if needed
+        return queryset
 
 
 class DormAmenitiesAssetUpdate(generics.UpdateAPIView):
