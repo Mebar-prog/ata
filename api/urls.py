233970f1a3a78f27a9af2ed319a2c49f5a42cc97
Mebar-Prog <sherabwang111@gmail.com>
2023-06-08 22:14:22 +0600

    PUT method url update

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from api.views import *
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -7,6 +7,8 @@ app_name = 'api'
 urlpatterns = [
     path('account/v1/generatetoken/', obtain_auth_token, name='api_token_auth'),
     path('assets/v1/dorm_amenities', DormAmenitiesAssetList.as_view(), name='dorm_amenities_asset_list'),
-    path('assets/v1/dorm_amenities/<str:pk>/', DormAmenitiesAssetUpdate.as_view(), name='dorm_amenities_asset_update'),
+    # path('assets/v1/dorm_amenities/<str:pk>/', DormAmenitiesAssetUpdate.as_view(), name='dorm_amenities_asset_update'),
+    re_path(r'^assets/v1/dorm_amenities/(?P<pk>.+)/$', DormAmenitiesAssetUpdate.as_view(), name='dorm_amenities_asset_update'),
+
 ]
 
